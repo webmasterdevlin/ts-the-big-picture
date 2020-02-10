f6848c8f8427296de751e3652e4d92870419828d
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-02-10 23:41:42 +0100

    updated

---
+++ build/basic.d.ts
@@ -0,0 +1,8 @@
+declare class Customer {
+    name: string;
+    constructor(name: string);
+    announce(): string;
+}
+declare let firstCustomer: Customer;
+declare let newMessage: string;
+declare let webHeading: HTMLHeadingElement | null;
